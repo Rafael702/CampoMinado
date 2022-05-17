e05b5d4a874051c075fdee9acee4b96ac0655d89
Rafael Santos <rafaeldealmeida.192001@gmail.com>
2022-05-17 20:49:40 -0300

    Criar Tela Principal.

---
+++ src/main/kotlin/visao/TelaPrincipal.kt
@@ -0,0 +1,41 @@
+package visao
+
+import modelo.Tabuleiro
+import modelo.TabuleiroEvento
+import javax.swing.JFrame
+import javax.swing.JOptionPane
+import javax.swing.SwingUtilities
+
+fun main(args: Array<String>) {
+    TelaPrincipal()
+}
+
+class TelaPrincipal : JFrame() {
+    private val tabuleiro = Tabuleiro(qtdLinhas = 16, qtdColunas = 30, qtdMinas = 89)
+    private val painelTabuleiro = PainelTabuleiro(tabuleiro)
+
+    init {
+        tabuleiro.onEvento(this::mostrarResultado)
+        add(painelTabuleiro)
+
+        setSize(690, 438)
+        setLocationRelativeTo(null)
+        defaultCloseOperation = EXIT_ON_CLOSE
+        title = "Campo Minado"
+        isVisible = true
+    }
+
+    private fun mostrarResultado(evento: TabuleiroEvento) {
+        SwingUtilities.invokeLater {
+            val msg = when (evento) {
+                TabuleiroEvento.VITORIA -> "Você Ganhou!"
+                TabuleiroEvento.DERROTA -> "Você Perdeu....:P"
+            }
+            JOptionPane.showMessageDialog(this, msg)
+            tabuleiro.reiniciar()
+
+            painelTabuleiro.repaint()
+            painelTabuleiro.validate()
+        }
+    }
+}
